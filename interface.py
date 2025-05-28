a2dc15eadb881f8d2f5917fb761e1bb41fd777ce
Valt2020shu <valt2020shu@gmail.com>
2025-05-28 23:19:57 +0530

    saveslot

--- interface.py
+++ interface.py
@@ -1,10 +1,14 @@
 
+saveslot1_name = "Number 1"
+saveslot2_name = "Number 2"
+
 def mainmenu ():
     print(" Welcome to Text-Based-RPG")
     print("", "(1) Play", "(2) Settings", "(3) Credits", sep = "\v\t", end = "\v\v\v")
     while True:
         choice = input("Enter input: ").lower().strip()
         if choice == "1" or choice == "play":
+            saveslot(saveslot1_name,saveslot2_name)
             print("Once upon a time")
             break
         elif choice == "2" or choice == "settings":
@@ -27,9 +31,20 @@ def quest(response,lore):
     print(f"Lore: {lore.text}")
 
 def saveslot(saveslot1_name, saveslot2_name):
-    print("Choose a saveslot: ")
+    
     print(f"Saveslot 1: {saveslot1_name}, Saveslot 2: {saveslot2_name}")
-    print('What do you want to do with the saveslot: ')
-    print("Play, Exit or Delete")
+    while True:
+        choice = input("Choose a Saveslot: ")
+        if choice == saveslot1_name or choice == saveslot2_name:
+            while True: 
+                choice2 = input('What do you want to do with the saveslot: \n Play, Exit or Delete: ' ).strip().lower()
+                if choice2 == "play" or choice2 == "delete" or choice2 == "exit":
+                    print(f"You chose to {choice2}")
+                    return
+                else:
+                    print("Please try again")
+            
+        else:   
+            print("Please try again")
 
 mainmenu()
