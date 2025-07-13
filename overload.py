052e1f90caee5580b011b84aec670e414928c827
vanrobo <155556005+vanrobo@users.noreply.github.com>
2025-07-13 15:20:09 +0530

    Delete overload.py

--- overload.py
+++
@@ -1,8 +0,0 @@
-import json
-
-sno = int(input("Enter a number from 1-4: "))
-
-with open(rf"Storage/Saveslots/{sno}/world.json", "r") as data:
-    map_data = json.load(data)
-    for tile in map_data['tiles']:
-        print(f"Tile ID: {tile['id']}, Category: {tile['category']}, Description: {tile['description']})")
